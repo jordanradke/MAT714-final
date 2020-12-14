a68a7ac604a7be8eba6f0f2e21314c0038500b49
jordanradke <72372638+jordanradke@users.noreply.github.com>
2020-12-14 16:49:24 -0600

    Update nusselt.m

--- nusselt.m
+++ nusselt.m
@@ -1,5 +1,17 @@
 % compute nusselt number given veloctiy/temperature profile
 function [nu] = nusselt(L,temp,v,a0,x2,dx3,sigma)
+% parameters:
+% L: an index, the cross section of box heat integrated over
+% temp: temp profile, matrix
+% v: vertical velocity profile, matrix
+% a0: width of channel
+% x2: horizontal grid
+% dx3: vertical grid size
+% sigma: Prandtl number
+%
+% returns:
+% nu: dimensionless Nusselt number
+
 
 % 1st order finite difference in x3 direction 
 dT    = 1/dx3*onesidediff(temp,L);
